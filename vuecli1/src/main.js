42fd9568a3b3b97de4dc2360baf0c8fe13516c60
zhengxiaolei <zhengxiaolei@vrvmail.com.cn>
2022-07-07 23:25:24 +0800

    ant design vue 和 阿里云矢量图库引入

--- vuecli1/src/main.js
+++ vuecli1/src/main.js
@@ -3,7 +3,20 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// ant-design-vue 引入组件库 引入样式表--
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+
+import { Icon } from "ant-design-vue";
+export default Icon.createFromIconfontCN({
+  scriptUrl: "../public/iconfont/iconfont.js",
+});
+
+//可以阻止 vue 在启动时生成生产提示
 Vue.config.productionTip = false;
+Vue.use(Antd);
+
+Vue.component("icon", "<div>12233</div>");
 
 new Vue({
   router,
--- vuecli1/src/views/Home.vue
+++ vuecli1/src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <a-button type="primary">Ant Design Vue</a-button>
+        <a-icon style="font-size:80px;color: #8be27e;" type="team" />
+        <span class="icon-54xiaoxi-1 iconfont" style="font-size:80px; color: #ea9518;"></span>
+        <icon class="icon" style="font-size:80px; color: #ea9518;" type="icon-54xiaoxi-1" />
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'home',
+    components: {
+        HelloWorld,
+    },
+};
 </script>
