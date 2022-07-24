acda07636e50f1cdd44327295d7d2e3a3db5478b
zhengxiaolei <zhengxiaolei@vrvmail.com.cn>
2022-07-24 18:19:49 +0800

    关键词练习

--- vuecli1/src/views/Home.vue
+++ vuecli1/src/views/Home.vue
@@ -24,9 +24,16 @@ export default {
         IconFont,
     },
     created() {
+        console.log('###############arrayEvery#########################');
         this.arrayEvery();
-        console.log('########################################');
+        console.log('##################arrMap######################');
         this.arrMap();
+        console.log('##################arrFilter######################');
+        this.arrFilter();
+        console.log('##################arrayIndexOf######################');
+        this.arrayIndexOf();
+        console.log('#################arrayFind#######################');
+        this.arrayFind();
     },
     methods: {
         /**
@@ -56,6 +63,36 @@ export default {
             });
             console.log(arrMap1);
             console.log(arrMap);
+        },
+        /**
+         * 返回满足条件的变量组成新数组
+         * 遍历全部
+         * **/
+        arrFilter() {
+            const arrFilter = [1, 2, 3, 4, 5, 6];
+            let arrFilter1 = arrFilter.filter((item, index, arr) => {
+                console.log('item:' + item + 'index:' + index + 'arr:' + arr);
+                return item > 3;
+            });
+            console.log('arrFilter1:' + arrFilter1);
+        },
+        /**
+         * 返回满足条件的变量下标
+         * **/
+        arrayIndexOf() {
+            const arrayIndexOf = [1, 2, 3, 4, 5, 6];
+            let num = arrayIndexOf.indexOf(3, 0); // 等同array.indexOf(3)
+            console.log('num:' + num);
+        },
+        /**
+         * 返回第一个满足条件的变量
+         * **/
+        arrayFind() {
+            const arrFind = [1, 2, 3, 4, 5];
+            let ret = arrFind.find((item) => {
+                return item > 3;
+            });
+            console.log('ret:' + ret);
         },
     },
 };
